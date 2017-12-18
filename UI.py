798a540393ed7591e00cb1742bd44267e19f5116
fengshidian <fengshidian@163.com>
2017-12-18 17:01:44 +0800

    12.18

--- UI.py
+++ UI.py
@@ -11,7 +11,7 @@ from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import seaborn as sns
 from QuantLib import *
-#import cal as cl
+import cal as cl
 
 class Calculation(QWidget):
 	def __init__(self):
@@ -21,8 +21,10 @@ class Calculation(QWidget):
 		self.CreateEdit()
 		self.CreateCombo()
 		self.CreateButton()
+		self.CreateCalendar()
 		self.comboBoxAct()
 		self.EditChange()
+		
 
 		self.initUI()
 	def CreateLabel(self):
@@ -34,75 +36,50 @@ class Calculation(QWidget):
 		self.lblUnderlyingPrice=QLabel('标的现价',self)
 
 		self.lblOptionPrice=QLabel('期权价格',self)
+		self.lblOptionType=QLabel('期权类型',self)
 
 		#self.lblCalculate=QLabel('开始计算',self)
 		#self.lblReCalculate=QLabel('重置',self)
 	def CreateButton(self):
 		self.btnCalculate=QPushButton('开始计算',self)
-		self.btnCalculate.clicked.connect(self.calculate)
+		self.btnCalculate.clicked.connect(self.btncalculate)
 		self.btnReCalculate=QPushButton('重置',self)
-		self.btnReCalculate.clicked.connect(self.recalculate)
+		self.btnReCalculate.clicked.connect(self.btnrecalculate)
+
+		self.btnStartDate=QPushButton('',self)
+		self.btnStartDate.clicked.connect(self.btnStartDateChange)
+
+		self.btnEndDate=QPushButton('',self)
+		self.btnEndDate.clicked.connect(self.btnEndDateChange)
 	def CreateEdit(self):
 		self.UnderlyingCodeEdit=QLineEdit(self)
+		self.UnderlyingCodeEdit.setText('204001')
 		self.UnderlyingNameEdit=QLineEdit(self)
+		self.UnderlyingNameEdit.setText('GC001')
 
 		self.StrikeEdit=QLineEdit(self)
 
 		self.UnderlyingPriceEdit=QLineEdit(self)
 		self.OptionPriceEdit=QLineEdit(self)
 	def CreateCombo(self):
-		self.comboStartYear=QComboBox(self)
-		self.comboEndYear=QComboBox(self)
-		self.comboStartYear.addItem('')
-		self.comboEndYear.addItem('')
-		for i in range(2016,2020):
-			self.comboStartYear.addItem(str(i))
-			self.comboEndYear.addItem(str(i))
-		
-		self.comboStartMonth=QComboBox(self)
-		self.comboEndMonth=QComboBox(self)
-		self.comboStartMonth.addItem('')
-		self.comboEndMonth.addItem('')
-		for i in range(1,13):
-			if len(str(i))==1:
-				self.comboStartMonth.addItem(str(0)+str(i))
-				self.comboEndMonth.addItem(str(0)+str(i))
-			else:
-				self.comboStartMonth.addItem(str(i))
-				self.comboEndMonth.addItem(str(i))
+	
 
-		self.comboStartDay=QComboBox(self)
-		self.comboEndDay=QComboBox(self)
-		self.comboStartDay.addItem('')
-		self.comboEndDay.addItem('')
-		for i in range(1,32):
-			if len(str(i))==1:
-				self.comboStartDay.addItem(str(0)+str(i))
-				self.comboEndDay.addItem(str(0)+str(i))
-			else:
-				self.comboStartDay.addItem(str(i))
-				self.comboEndDay.addItem(str(i))
+		self.comboType=QComboBox(self)
+		self.comboType.addItem('')
+		self.comboType.addItem('call')
+		self.comboType.addItem('put')
+	def CreateCalendar(self):
+		self.calStartDate=QCalendarWidget(self)
+		self.calStartDate.hide()
+		self.calEndDate=QCalendarWidget(self)
+		self.calEndDate.hide()
 	
 	def comboBoxAct(self):
-		self.comboStartYear.activated[str].connect(self.onActivatedStartYear)
-		self.comboStartMonth.activated[str].connect(self.onActivatedStartMonth)
-		self.comboStartDay.activated[str].connect(self.onActivatedStartDay)
-		
-		self.comboEndYear.activated[str].connect(self.onActivatedEndYear)
-		self.comboEndMonth.activated[str].connect(self.onActivatedEndMonth)
-		self.comboEndDay.activated[str].connect(self.onActivatedEndDay)
+
+		self.comboType.activated[str].connect(self.onActivatedType)
 	def EditChange(self):
 		self.StrikeEdit.textChanged[str].connect(self.StrikeChange)
 	def initUI(self):
-		comboStartDate=QHBoxLayout()
-		comboStartDate.addWidget(self.comboStartYear)
-		comboStartDate.addWidget(self.comboStartMonth)
-		comboStartDate.addWidget(self.comboStartDay)
-
-		comboEndDate=QHBoxLayout()
-		comboEndDate.addWidget(self.comboEndYear)
-		comboEndDate.addWidget(self.comboEndMonth)
-		comboEndDate.addWidget(self.comboEndDay)
 
 
 
@@ -110,21 +87,27 @@ class Calculation(QWidget):
 		grid.addWidget(self.lblUnderlyingCode,0,0)
 		grid.addWidget(self.UnderlyingCodeEdit,0,1)
 		grid.addWidget(self.lblStartDate,0,2)
-		grid.addLayout(comboStartDate,0,3)
+		grid.addWidget(self.btnStartDate,0,3)
+		grid.addWidget(self.calStartDate,0,3)
+
 		grid.addWidget(self.lblUnderlyingPrice,0,4)	
 		grid.addWidget(self.UnderlyingPriceEdit,0,5)
 		grid.addWidget(self.lblUnderlyingName,1,0)
 		grid.addWidget(self.UnderlyingNameEdit,1,1)
 		grid.addWidget(self.lblEndDate,1,2)
-		grid.addLayout(comboEndDate,1,3)
+		grid.addWidget(self.btnEndDate,1,3)
+		grid.addWidget(self.calEndDate,1,3)			
+	
 		grid.addWidget(self.lblStrike,1,4)
 		grid.addWidget(self.StrikeEdit,1,5)
 		
-		grid.addWidget(self.lblOptionPrice,2,0)
-		grid.addWidget(self.OptionPriceEdit,2,1)
+		grid.addWidget(self.lblOptionPrice,3,0)
+		grid.addWidget(self.OptionPriceEdit,3,1)
+		grid.addWidget(self.lblOptionType,2,0)
+		grid.addWidget(self.comboType,2,1)		
 		
-		grid.addWidget(self.btnCalculate,2,4)
-		grid.addWidget(self.btnReCalculate,2,5)
+		grid.addWidget(self.btnCalculate,3,4)
+		grid.addWidget(self.btnReCalculate,3,5)
 		
 		vbox=QVBoxLayout()
 		vbox.addLayout(grid)
@@ -135,31 +118,71 @@ class Calculation(QWidget):
 		self.setWindowTitle('option calculation')
 		self.setGeometry(300,300,900,300)
 
-	def calculate(self):
-		pass
-	def recalculate(self):
-		pass
-	def onActivatedStartYear(self,StartYear):
-		self.StartYear=StartYear
-		self.StartDate=self.StartYear+self.StartDate[-4:]
-	def onActivatedStartMonth(self,StartMonth):
-		self.StartMonth=StartMonth
-		self.StartDate=self.StartDate[:4]+self.StartMonth+self.StartDate[-2:]
-	def onActivatedStartDay(self,StartDay):
-		self.StartDay=StartDay
-		self.StartDate=self.StartDate[:6]+self.StartDay
+	def btncalculate(self):
+		parameters=cl.Paras()
+		sigma=parameters.sigma[0]
+		alpha=parameters.alpha[0]
+		mu=parameters.mu[0]
+		spot=parameters.GC.iloc[-1][0]
 
-	def onActivatedEndYear(self,EndYear):
-		self.EndYear=EndYear
-		self.EndDate=self.EndYear+self.EndDate[-4:]
-	def onActivatedEndMonth(self,EndMonth):
-		self.EndMonth=EndMonth
-		self.EndDate=self.EndDate[:4]+self.EndMonth+self.EndDate[-2:]
-	def onActivatedEndDay(self,EndDay):
-		self.EndDay=EndDay
-		self.EndDate=self.EndDate[:6]+self.EndDay
-	def StrikeChange(self):
-		pass
+		min_=parameters.min
+		optionType=self.OptionType
+		strike=self.strike
+		china_calendar=China()
+		
+		tradedays=china_calendar.businessDaysBetween(self.st_date,self.ed_date)
+
+		ML=cl.Model(alpha,mu,spot,sigma,tradedays,strike,10000,optionType,min_)
+		self.OptionPriceEdit.setText(str(ML.price))
+	def btnrecalculate(self):
+		self.OptionPriceEdit.setText('')
+	def btnStartDateChange(self):
+		self.calStartDate.show()
+		self.calStartDate.setGridVisible(True)
+		self.calStartDate.resize(2,2)
+		self.calStartDate.clicked[QDate].connect(self.showStartDate)
+		
+	def showStartDate(self,date):
+		txt=date.toString()
+		year=int(txt[-4:])
+		day=int(txt[-7:-5])
+		if (txt[3:5][-1]==u'月'):
+			month=int(txt[3])
+		else:
+			month=int(txt[3:5])
+		self.st_date=Date(day,month,year)
+
+		self.btnStartDate.setText(str(year)+'/'+str(month)+'/'+str(day))
+
+		
+		self.calStartDate.hide()
+	def btnEndDateChange(self):
+		self.calEndDate.show()
+		self.calEndDate.setGridVisible(True)
+		self.calEndDate.resize(2,2)
+		self.calEndDate.clicked[QDate].connect(self.showEndDate)
+		
+		
+	def showEndDate(self,date):
+		txt=date.toString()
+		year=int(txt[-4:])
+		day=int(txt[-7:-5])
+		if (txt[3:5][-1]==u'月'):
+			month=int(txt[3])
+		else:
+			month=int(txt[3:5])
+		self.ed_date=Date(day,month,year)
+		
+		self.btnEndDate.setText(str(year)+'/'+str(month)+'/'+str(day))
+		self.calEndDate.hide()
+		
+		
+		
+
+	def onActivatedType(self,Type):
+		self.OptionType=Type
+	def StrikeChange(self,strike):
+		self.strike=float(strike)
 if __name__=='__main__':
 	#sns.set(color_codes=True)
 	app=QApplication(sys.argv)
--- cal.py
+++ cal.py
@@ -1,43 +1,70 @@
 import numpy as np
 import random
-
+import pandas as pd
 class Model:
-    def __init__(self,a,b,r0,sigma,tradedays,strike,numberPaths):
-        self.a=a
-        self.b=b
-        self.r0=r0
-        self.sigma=sigma
-        self.tradedays=tradedays
-        self.strike=strike
-        self.numberPaths=numberPaths
-        self.callsimulation()
-        self.putsimulation()
-    def callsimulation(self):
-        self.PathPrice=[]
-        for j in range(self.numberPaths-1):
-            self.r=[self.r0]
-            for i in range(self.tradedays-1):
-                self.r.append(self.r[-1]+self.a*(self.b-self.r[-1])+self.sigma*np.random.randn())
-            temp=np.sum(self.r[1:])
-            self.payoff=(temp/self.tradedays-self.strike)
-            if(self.payoff>0):
-                self.payoff=self.payoff*100.0
-            else:
-                self.payoff=0
-            self.PathPrice.append(self.payoff*np.exp(-self.r[-1]*(self.tradedays/360)))
-        self.callprice=np.mean(self.PathPrice)
-    def putsimulation(self):
-        self.PathPrice=[]
-        for j in range(self.numberPaths-1):
-            self.r=[self.r0]
-            for i in range(self.tradedays-1):
-                self.r.append(self.r[-1]+self.a*(self.b-self.r[-1])+self.sigma*np.random.randn())
-            temp=np.sum(self.r[1:])
-            self.payoff=(self.strike-temp/self.tradedays)
-            if(self.payoff>0):
-                self.payoff=self.payoff*100.0
-            else:
-                self.payoff=0
-            self.PathPrice.append(self.payoff*np.exp(-self.r[-1]*(self.tradedays/360)))
-        self.putprice=np.mean(self.PathPrice)
-        
+	def __init__(self,a,b,r0,sigma,tradedays,strike,numberPaths,optiontype,min_):
+		self.a=a
+        	self.b=b
+       		self.r0=r0
+        	self.sigma=sigma
+        	self.tradedays=tradedays
+        	self.strike=strike
+        	self.numberPaths=numberPaths
+        	self.optiontype=optiontype
+        	self.count=0.0
+        	self.min_=min_
+        	self.simulation()
+	def simulation(self):
+        	self.PathPrice=[]
+        	for j in range(self.numberPaths-1):
+            		self.r=[self.r0]
+            		for i in range(self.tradedays-1):
+                		temp=self.r[-1]+self.a*(self.b-self.r[-1])+self.sigma*np.sqrt(self.r[-1])*np.random.randn()
+                		if temp>0:
+                    			self.r.append(temp)
+                		else:
+                    			self.r.append(self.min_)
+                    			self.count=self.count+1.0
+            		temp=np.sum(self.r[1:])
+            		if self.optiontype=='call':
+                		self.payoff=(temp/self.tradedays-self.strike)
+            		elif self.optiontype=='put':
+                    		self.payoff=(self.strike-temp/self.tradedays)
+            		else:
+                		print 'enter the right option type'
+            		if(self.payoff>0):
+                		self.payoff=self.payoff*100.0
+            		else:
+                		self.payoff=0
+            		self.PathPrice.append(self.payoff*np.exp(-self.r[-1]*(self.tradedays/360.0)))
+        	self.price=np.mean(self.PathPrice)
+
+class Paras:
+	def __init__(self):
+		self.paras()
+	def paras(self):
+		data=pd.read_excel("GC001.xlsx")
+		GC_index=pd.DatetimeIndex(data[3:].index)
+		GC=pd.DataFrame(np.matrix(data[3:])/100.0,index=GC_index,columns=['spot'])
+		self.GC=GC
+		GC_average=np.mean(GC)
+		GC_vol=np.std(GC)
+		delta_T=np.sum(GC.diff(1)**2)
+		self.sigma=np.sqrt(delta_T/(np.sum(GC[:-1])))
+		self.mu=GC_average
+		self.alpha=(self.sigma**2)*self.mu/(2*GC_vol)
+		self.min=np.mean(GC)[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
